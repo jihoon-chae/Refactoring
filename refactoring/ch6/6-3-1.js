1a0e01fdbc398de7615c720274f0cd5260c81013
Jihoon Chae <93469760+jihoon-chae@users.noreply.github.com>
2022-08-09 11:00:36 +0900

    Add files via upload

--- refactoring/ch6/6-3-1.js
+++ refactoring/ch6/6-3-1.js
@@ -1,8 +1,10 @@
 export function price(order) {
   // 가격(price) = 기본가격 - 수량할인 + 배송비
-  return (
-    order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  );
+
+  const basePrice = order.quantity * order.itemPrice; // 수량과 가격의 곱
+  const discount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+  return basePrice - discount + shipping;
 }
+
+// 긴 표현식은 잘게 잘라쓰는게 가독성이 더 좋음
