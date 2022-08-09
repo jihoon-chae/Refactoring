3a58f5cbe1a553e64f3128518d2f7ac1e825d937
Jihoon Chae <93469760+jihoon-chae@users.noreply.github.com>
2022-08-09 10:45:12 +0900

    함수 인라인하기

--- refactoring/ch6/6-2.js
+++ refactoring/ch6/6-2.js
@@ -1,20 +1,18 @@
 // 예제 1
 export function rating(driver) {
-  return moreThanFiveLateDeliveries(driver) ? 2 : 1;
+  return driver.numberOfLateDeliveries > 5 ? 2 : 1; // 점수로 등급 매기기
 }
 
-function moreThanFiveLateDeliveries(dvr) {
-  return dvr.numberOfLateDeliveries > 5;
-}
+function moreThanFiveLateDeliveries(dvr) {}
 
 // 예제 2
+// 지역변수 하나만들어서 데이터를 반환 => 불필요한 함수, 그냥 호출하면 됨
+// 불필요한 간접호출 일어나지 않도록 가지고 와서 인라인 해줌
 function reportLines(customer) {
-  const lines = [];
-  gatherCustomerData(lines, customer);
-  return lines;
+  const result = [];
+  result.push(["name", customer.name]);
+  result.push(["location", customer.location]);
+  return result;
 }
 
-function gatherCustomerData(out, customer) {
-  out.push(['name', customer.name]);
-  out.push(['location', customer.location]);
-}
+// 긴코드는 의미있는 단위로 나눠서 함수를 만들고 ㅇ
